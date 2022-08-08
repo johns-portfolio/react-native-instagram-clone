8a8eb4901fc09a5e168ee4689e85adfa178bb212
Apisit Sianglert <apisit.sianglert@lotuss.com>
2022-08-08 17:05:45 +0700

    Add screen navigator

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import Home from './screens/Home'
 import NewPost from './screens/NewPost'
+import Navigation from './Navigation'
 
 export default function App() {
-  return <NewPost />
+  return <Navigation />
 }
---
+++ Navigation.js
@@ -0,0 +1,22 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Home from './screens/Home'
+import NewPost from './screens/NewPost'
+
+const Stack = createNativeStackNavigator()
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name='NewPost' component={NewPost} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
